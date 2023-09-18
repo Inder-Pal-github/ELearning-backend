985bd0e77ffcaa30fab9b34fa5ea31d9703e3e86
Inder-Pal-github <inder39811@gmail.com>
2023-09-18 23:04:55 +0530

    [Registration]: functionality complete with activation code verification and user registration

--- controllers/user.controller.ts
+++ controllers/user.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response, NextFunction } from "express";
-import UserModel from "../models/user.model";
+import UserModel, { IUser } from "../models/user.model";
 import ErrorHandler from "../utils/ErrorHandler";
 import { CatchAsyncErrors } from "../middlewares/catchAsyncError";
 import jwt, { Secret } from "jsonwebtoken";
@@ -75,3 +75,47 @@ export const createActivationToken = (user: any): IActivationToken => {
   );
   return { token, activationCode };
 };
+
+// activate user
+interface IActivationRequest {
+  activation_token: string;
+  activation_code: string;
+}
+
+export const activateUser = CatchAsyncErrors(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { activation_token, activation_code } =
+        req.body as IActivationRequest;
+
+      const newUser: { user: IUser; activationCode: string } = jwt.verify(
+        activation_token,
+        process.env.ACTIVATION_SECRET as string
+      ) as { user: IUser; activationCode: string };
+        console.log("New user",newUser)
+      // verify activation code and send res
+      if (newUser.activationCode !== activation_code) {
+        return next(new ErrorHandler("Invalid activation code", 400));
+      }
+
+      const { name, email, password } = newUser.user;
+
+      // check if user exist
+      const existUser = await UserModel.findOne({ email });
+      if (existUser) {
+        return next(new ErrorHandler("Email already exists!", 400));
+      }
+
+      const user = await UserModel.create({
+        name,
+        email,
+        password,
+      });
+      res.status(201).json({
+        success: true,
+      });
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 400));
+    }
+  }
+);
